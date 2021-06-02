58ba546c70469ef1312de45ef2c54edadeb618c5
Paul Tonning <paul@dotclientsolutions.com>
2021-06-01 18:55:19 -0700

    initial

---
+++ source_code_step_5/space_rocks/models.py
@@ -0,0 +1,54 @@
+from pygame.math import Vector2
+from pygame.transform import rotozoom
+
+from utils import load_sprite, wrap_position
+
+UP = Vector2(0, -1)
+
+
+class GameObject:
+    def __init__(self, position, sprite, velocity):
+        self.position = Vector2(position)
+        self.sprite = sprite
+        self.radius = sprite.get_width() / 2
+        self.velocity = Vector2(velocity)
+
+    def draw(self, surface):
+        blit_position = self.position - Vector2(self.radius)
+        surface.blit(self.sprite, blit_position)
+
+    def move(self, surface):
+        self.position = wrap_position(self.position + self.velocity, surface)
+
+    def collides_with(self, other_obj):
+        distance = self.position.distance_to(other_obj.position)
+        return distance < self.radius + other_obj.radius
+
+
+class Spaceship(GameObject):
+    MANEUVERABILITY = 3
+    ACCELERATION = 0.25
+
+    def __init__(self, position):
+        # Make a copy of the original UP vector
+        self.direction = Vector2(UP)
+        self.zoom = 1.0
+
+        super().__init__(position, load_sprite("spaceship3-10"), Vector2(0))
+
+    def rotate(self, clockwise=True):
+        sign = 1 if clockwise else -1
+        zoom = 0.01 if clockwise else -0.01
+        self.zoom = self.zoom + self.zoom*zoom
+        angle = self.MANEUVERABILITY * sign
+        self.direction.rotate_ip(angle)
+
+    def accelerate(self):
+        self.velocity += self.direction * self.ACCELERATION
+
+    def draw(self, surface):
+        angle = self.direction.angle_to(UP)
+        rotated_surface = rotozoom(self.sprite, angle, self.zoom)
+        rotated_surface_size = Vector2(rotated_surface.get_size())
+        blit_position = self.position - rotated_surface_size * 0.5
+        surface.blit(rotated_surface, blit_position)
